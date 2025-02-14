da052affe44fb6f2162713c4842e9a2dbdfc9afa
sergiolttz <sergiolattanzio05@gmail.com>
2025-02-14 10:50:29 -0400

    urls and views done

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('lista/', Lista_de_publicaciones, name='lista'),
+    path('<str:titulo_arg>/<str:tema_arg>/', Detalle_publicacion, name='detalles'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import Post
 
-# Create your views here.
+def Lista_de_publicaciones(request):
+    publicaciones = get_list_or_404(Post)
+    publicaciones = Post.objects.all() 
+    return render(request, 'lista_publicaciones.html', {'publicaciones': publicaciones})
+
+def Detalle_publicacion(request, titulo_arg, tema_arg):
+    publicacion = get_list_or_404(Post, titulo=titulo_arg, tema=tema_arg)
+    return render(request, 'post_unico.html', {'publicacion': publicacion})    
