c97c2dca4849706fef564524121377eace3493e7
sergiolttz <sergiolattanzio05@gmail.com>
2025-02-17 15:41:15 -0400

    vista basada en clases

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = 'blog'
 
 urlpatterns = [
-    path('lista/', Lista_de_publicaciones, name='lista'),
+    path('lista/', Lista_de_publicaciones.as_view(), name='lista'),
     path('<str:titulo_arg>/<str:tema_arg>/', Detalle_publicacion, name='detalles'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+'''from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger'''
+from django.views.generic import ListView
 
+class Lista_de_publicaciones(ListView):
+    model= Post
+    context_object_name = "publicaciones"
+    paginate_by = 2
+    template_name = "lista_posts.html"
+
+
+'''
 def Lista_de_publicaciones(request):
     paginacion = Paginator(Post.objects.all(), 2)
     pagina = request.GET.get('pagina')
@@ -14,11 +23,9 @@ def Lista_de_publicaciones(request):
         publicaciones = paginacion.page(paginacion.num_pages)
     return render(request, 'lista_posts.html', {'publicaciones': publicaciones})
 
-    '''
-    publicaciones = get_list_or_404(Post)
-    publicaciones = Post.objects.all() 
-    return render(request, 'lista_posts.html', {'publicaciones': publicaciones})
-    '''
+    
+'''
+
 
 def Detalle_publicacion(request, titulo_arg, tema_arg):
     publicacion = get_object_or_404(Post, titulo=titulo_arg, tema=tema_arg)
