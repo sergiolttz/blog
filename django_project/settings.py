f36c33bec5dcce27d5bdd35d3ffb643232f8d3b9
sergiolttz <sergiolattanzio05@gmail.com>
2025-02-21 11:56:13 -0400

    conectado a base de datos blog

--- django_project/settings.py
+++ django_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog",
+        "USER": "postgres",
+        "PASSWORD": "123456",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
